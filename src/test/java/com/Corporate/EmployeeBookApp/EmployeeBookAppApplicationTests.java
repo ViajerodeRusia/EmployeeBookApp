5c3de7cafe92f20bd4290b0483072fc365bdd81d
Denis Siliukov <abhem21rus@mail.ru>
2024-01-09 10:22:55 +0300

    Project initialization.

---
+++ src/test/java/com/Corporate/EmployeeBookApp/EmployeeBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Corporate.EmployeeBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
